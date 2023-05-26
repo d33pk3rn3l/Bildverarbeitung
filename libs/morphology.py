fec4d44506e1c6c2f720000663f1ae43ea0551bc
d33pk3rn3l <5982817+d33pk3rn3l@users.noreply.github.com>
2023-05-27 01:49:08 +0200

    kinda works

--- libs/morphology.py
+++ libs/morphology.py
@@ -55,8 +55,17 @@ def close_hole_util(img):
     Output:
         closed_img: n x m
     """
-    ## TODO
-    closed_img = ...
+    # Apply dilation
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    dilated_img = cv2.dilate(img, kernel, iterations=1)
+
+    # Apply erosion
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    closed_img = cv2.erode(dilated_img, kernel, iterations=5)
+
+    # Apply dilation
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    closed_img = cv2.dilate(closed_img, kernel, iterations=5)
 
     return closed_img
 
@@ -68,8 +77,39 @@ def instance_segmentation_util(img):
     Output:
         instance_seg_img:    n x m x 3, different coin instances have different colors
     """
-    ## TODO
-    instance_seg_img = ...
+    # Apply dilation
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    dilated_img = cv2.dilate(img, kernel, iterations=2)
+
+    # Apply erosion
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(100, 100))
+    closed_img = cv2.erode(dilated_img, kernel, iterations=2)
+
+    for _ in range(10):
+        closed_img = cv2.GaussianBlur(closed_img, (51, 51), 10)
+        # Re-threshold the image to make it binary again
+        _, closed_img = cv2.threshold(closed_img, 127, 255, cv2.THRESH_BINARY)
+
+    # Apply dilation
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    closed_img = cv2.dilate(closed_img, kernel, iterations=40)
+
+    # Find connected components
+    _, labeled_img = cv2.connectedComponents(closed_img)
+    labeled_img += 1
+
+    closed_img = cv2.merge((closed_img, closed_img, closed_img))
+    watershed = cv2.watershed(closed_img, labeled_img)
+
+    for segment in np.unique(watershed):
+        closed_img[watershed == segment] = [255 * random.random(),
+                                            255 * random.random(),
+                                            255 * random.random()]
+
+    instance_seg_img = closed_img * 255
+
+    ### Better to use https://docs.opencv.org/4.x/d3/db4/tutorial_py_watershed.html
+    ### to do the instance segmentation, but I thought I might try something different
 
     return instance_seg_img
 
@@ -85,6 +125,20 @@ def text_recog_util(text, letter_not):
     from scipy.ndimage import binary_erosion as erode
     from scipy.ndimage import binary_dilation as dilate
     ## TODO
-    text_er_dil = ...
+    # Initial image thresholding
+    # We apply binary inverse thresholding to get the binary representations of the images
+    _, text_binary = cv2.threshold(text, 0.5, 1, cv2.THRESH_BINARY_INV)
+    _, letter_binary = cv2.threshold(letter_not, 0.5, 1, cv2.THRESH_BINARY)
+
+    # Perform binary erosion on the text_binary with the structuring element being letter_binary
+    # Erosion will shrink the white regions (foreground) in the binary image
+    eroded_image = binary_erosion(text_binary, letter_binary)
+
+    # Perform binary dilation on the eroded image with the structuring element being letter_binary
+    # Dilation will expand the white regions in the binary image
+    dilated_image = binary_dilation(eroded_image, letter_binary)
+
+    # Our final image is the dilated image
+    text_er_dil = dilated_image
 
     return text_er_dil
